d0f5ae75997b4c88e54bcd499c02d2dc3100cb0a
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2018-10-28 10:55:15 +0100

    added vuex + vue-i18n

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  runtimeCompiler: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'languages',
+      enableInSFC: false
+    }
+  }
+}

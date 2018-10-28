cc9102bf6f08972d8c55942bd3453cc12e11c1bc
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2018-10-28 11:01:41 +0100

    re-install vue-i18n

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  runtimeCompiler: true,
+	runtimeCompiler: true,
 
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'languages',
-      enableInSFC: false
-    }
-  }
-}
+	pluginOptions: {
+		i18n: {
+			locale: 'en',
+			fallbackLocale: 'en',
+			localeDir: 'locales',
+			enableInSFC: false
+		}
+	}
+};
